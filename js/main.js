3f0c7d0423d7211fba26e767a6c4f26354805384
Alexander Shaw <shou.aa00@gmail.com>
2018-11-01 17:50:35 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,35 +1,66 @@
 //var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const catApiKey = '2dc76a41-d118-4c29-9b52-95f402be4611'; 
+const catApiKey = '2dc76a41-d118-4c29-9b52-95f402be4611';
 const apiUrl = 'https://api.thecatapi.com/v1';
+const corsUrl = 'https://bypasscors.herokuapp.com/api/?url=';
+const noBlockMeApiUrl = 'http://proxy9747.my-addr.org/myaddrproxy.php/';
 const ASYNC_ENABLED = true;
-const IMAGES_SEARCH = '/images/search'; 
+const IMAGES_SEARCH = '/images/search';
 
-
-function getRandomCatPicFor(img)
+function anonymizeUrl(url)
 {
-    var result; 
-    const http = new XMLHttpRequest(); 
-    http.open("GET", apiUrl + IMAGES_SEARCH + "?", ASYNC_ENABLED); 
-    http.setRequestHeader("x-api-key", catApiKey); 
-    http.onreadystatechange = function()
+    var protocol = url.substring(0, url.indexOf(':'));
+    var urlNoProtocol = url.replace(/^https?\:\/\//i, "");
+    var resultUrl = noBlockMeApiUrl + protocol + "/" + urlNoProtocol;
+
+    return (resultUrl);
+}
+
+function getRandomCatPicFor(img, picSize = "medium", needGifs = true, needStaticImages = true)
+{
+    var paramSize = picSize == "medium" ? "med" : picSize;
+    var paramMimeTypes = "";
+    if (needGifs)
+    {
+        paramMimeTypes += "gif,";
+    }
+    if (needStaticImages)
+    {
+        paramMimeTypes += "jpg,png,";
+    }
+    if (!paramMimeTypes)
+    {
+        return null;
+    }
+    paramMimeTypes = paramMimeTypes.slice(0, -1);
+
+    const http = new XMLHttpRequest();
+    http.open("GET", apiUrl + IMAGES_SEARCH + "?size=" + paramSize + "&mime_types=" + paramMimeTypes, ASYNC_ENABLED);
+    console.log("[" + apiUrl + IMAGES_SEARCH + "?size=" + paramSize + "&mime_types=" + paramMimeTypes + "]");
+    http.setRequestHeader("x-api-key", catApiKey);
+    http.onreadystatechange = function ()
     {
         if (http.readyState != 4)
         {
-            return; 
+            return;
         }
         else if (http.status != 200)
         {
-            console.log("error: " + http.statusText); 
+            console.log("error: " + http.statusText);
+            return;
         }
 
-        var jsonResponse = JSON.parse(http.responseText); 
-        var picUrl = jsonResponse[0].url; 
-
-        result = picUrl; 
-
-        img.src = (picUrl + "?random" + new Date().getTime());
-
+        var jsonResponse = JSON.parse(http.responseText);
+        var picUrl = jsonResponse[0].url;
+        console.log("original url=[" + picUrl + "]");
+        if(picUrl.search("tumblr"))
+        {
+            img.src = anonymizeUrl(picUrl);
+        }
+        else
+        {
+            img.src = picUrl;
+        }
     }
 
     http.send();
@@ -37,5 +68,5 @@ function getRandomCatPicFor(img)
 
 function print(value)
 {
-    console.log(value); 
+    console.log(value);
 }
